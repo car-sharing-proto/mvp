8e3c892bb55316fa989e6f66a1ed1ce14a0fdb76
moxcelix <maxim.spf@gmail.com>
2023-12-06 15:05:58 +0300

    dotenv is implemented

--- app/infrastructure/db_user_repository.py
+++ app/infrastructure/db_user_repository.py
@@ -1,23 +1,24 @@
 import psycopg2
+import os
+
+from dotenv import load_dotenv
 from app.models.user import User
 
 class UserRepository():
     def __init__(self) -> None:
-        pass
+        load_dotenv()
 
     # returns user by id if exists
     def get_user(self, id):
         con = psycopg2.connect(
-                dbname="carsharing",
-                user="postgres",
-                password="1111",
-                host="host.docker.internal",
-                port=5432
+                dbname=os.environ['DB_NAME'],
+                user=os.environ['DB_USER'],
+                password=os.environ['DB_PASS'],
+                host=os.environ['SERVER_HOST'],
+                port=os.environ['DB_PORT']
             )
         cur = con.cursor()
-
         cur.execute('SELECT * FROM TUser WHERE (_id = {0});'.format(id))
-        
         data = cur.fetchone()
 
         if(data is None):
@@ -30,23 +31,22 @@ class UserRepository():
             str(user_name), 
             str(user_pass)
         )
-
         con.close()
-        
         return user
     
     # add user to db
     def add_user(self, user):
         con = psycopg2.connect(
-                dbname="carsharing",
-                user="postgres",
-                password="1111",
-                host="host.docker.internal",
-                port=5432
+                dbname=os.environ['DB_NAME'],
+                user=os.environ['DB_USER'],
+                password=os.environ['DB_PASS'],
+                host=os.environ['SERVER_HOST'],
+                port=os.environ['DB_PORT']
             )
         cur = con.cursor()
         cur.execute(
-            "INSERT INTO TUser (_id, _role, _name, _pass) VALUES ({0}, '{1}', '{2}', '{3}');".format(
+            "INSERT INTO TUser (_id, _role, _name, _pass) "+
+            "VALUES ({0}, '{1}', '{2}', '{3}');".format(
                 user.id,
                 user.role, 
                 user.name,
